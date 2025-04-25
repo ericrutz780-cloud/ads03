dda5f7e6ee7bbc540692fb89ec308df2e5be3301
ErRutz01 <eric.rutz@student.dbuas.de>
2025-04-25 03:46:28 +0200

    Aenderung der import-data.sh

--- import-data.sh
+++ import-data.sh
@@ -1,5 +1,10 @@
+echo "[INFO] Der input Ordner wird gelöscht
 rm -rf input
+echo "[INFO] Ein neuer input Ordner wird erstellt
 mkdir input
+echo "[INFO] In den input Ordner navigieren"
 cd input
+echo "[INFO] Mit wget über der GitHub URL den titanic csv Datensetz importieren"
 wget https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/titanic.csv
+echo "[INFO] Aus dem input Ordner raus navigieren"
 cd ..
